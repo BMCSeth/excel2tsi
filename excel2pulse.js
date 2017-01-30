2a4dd5ba6de3e7f931fa34dd4b680fee8cb16afc
tsguru <martin.tauber@rocketmail.com>
2017-01-30 13:49:25 +0100

    moved tsiapi code to seperate file

--- excel2pulse.js
+++ excel2pulse.js
@@ -1,176 +1,9 @@
 var XLSX = require('xlsx');
-var https = require('https');
 var log4js = require('log4js');
 var logger = log4js.getLogger();
 
-/**
- * This class implements the interface to TrueSight Pulse. You need to
- * instanciate this class to be able to communicate with the TrueSight pulse
- * server. During the instanciatetion you need to specify the options
- *   email - the email address used to identify at the truesight server
- *   apiToken - the apiToken used to identify at the truesight server
- *
- * optionally you can specify the following options:
- *   hostname - the hostname of the truesight server
- *   port - the portnumber of the truesight server
- *   handler - the handler to be used to handle requests, responces and errors
- *
- * @class
- * @constructor
- * @param {object} options
- * @author Martin Tauber <martin_tauber@bmc.com>
- */
-function TSPulseAPI(options) {
-  this.email = options.email;
-  this.apiToken = options.apiToken;
-  this.hostname = options.hostname != undefined ? options.hostname : 'api.truesight.bmc.com';
-  this.port = options.port != undefined ? options.port : 443;
-  this.handler = options.handler != undefined ? options.handler : new TSPulseAPIDefaultHandler();
+var tsiapi = require('./tsiapi');
 
-  this.statistics = {
-    numberOfRequests: 0,
-    numberOfResponces: 0,
-    numberOfErrors: 0,
-    numberOfFatalErrors: 0
-//    firstRequestAt
-//    lastRequestAt
-//    firstResponceAt
-//    lastResponceAt
-  }
-
-  var self=this;
-
-  this.options = {
-    protocol: 'https:',
-    hostname: this.hostname,
-    port: this.port,
-    auth: this.email + ':' + this.apiToken,
-    headers: {
-        'Content-Type': 'application/json',
-    }
-  };
-
-  this._request = function(data) {
-    if (self.statistics.firstRequestAt == undefined)
-      self.statistics.firstRequestAt = new Date();
-
-    self.statistics.lastRequestAt = new Date();
-
-    var request = https.request(self.options, function(result) {
-      self.statistics.numberOfResponces++;
-      if (self.statistics.firstResponceAt == undefined)
-        self.statistics.firstResponceAt = new Date();
-
-      self.statistics.lastResponceAt = new Date();
-
-      if (result.statusCode >= 400) {
-        self.statistics.numberOfErrors++;
-      }
-
-      self.handler.handleResponce(self, result);
-    });
-
-    request.on('error', function(e) {
-      self.statistics.numberOfFatalErrors++;
-      self.handler.handleError(e);
-    });
-
-    request.write(JSON.stringify(data));
-    request.end();
-
-    self.statistics.numberOfRequests++;
-    self.handler.handleRequest(self);
-  }
-}
-
-TSPulseAPI.prototype.createEvent = function(data) {
-  this.options.path = '/v1/events';
-  this.options.method = 'POST';
-
-  this._request(data);
-}
-
-/**
- * @author Martin Tauber <martin_tauber@bmc.com>
- */
-TSPulseAPI.prototype.createEvents = function(dataProvider, options) {
-  logger.info("Creating events ...");
-  if (options == undefined) options = {};
-
-  var ratio = options.ratio == undefined ? 80 : options.ratio;
-  var min = options.min == undefined ? 50 : options.min;
-  var max = options.max == undefined ? 150 : options.max;
-
-  var self = this;
-
-  var worker = function(dataProvider, ratio) {
-    var i = 0;
-    while (i < ratio) {
-      data = dataProvider.next();
-      if (typeof data === 'undefined') break;
-        self.createEvent(data);
-      i++;
-    }
-
-    // selftune the ratio
-    var backlog = self.statistics.numberOfRequests - self.statistics.numberOfResponces;
-    if ( backlog > max ) {
-      ratio = Math.max(0, ratio - (backlog - max));
-    } else if ( backlog + 10 < min + (max - min) / 2) {
-      ratio = ratio + 10;
-    } else if ( backlog - 10 > min + (max - min) / 2) {
-      ratio = ratio - 10;
-    }
-    // logger.debug(min + ":" + max + " " + backlog + " " + ratio);
-    self.ratio = ratio;
-
-    if (typeof data !== 'undefined') setTimeout(worker, 1000, dataProvider, ratio);
-  }
-
-  worker(dataProvider, ratio);
-}
-
-
-/**
- * @author Martin Tauber <martin_tauber@bmc.com>
- */
-function TSPulseAPIDefaultHandler() {
-}
-
-/**
- * @author Martin Tauber <martin_tauber@bmc.com>
- */
-TSPulseAPIDefaultHandler.prototype.handleRequest = function(handle) {
-  if (handle.statistics.numberOfRequests % 1000 == 0) {
-    logger.info("Requests: " + handle.statistics.numberOfRequests +
-       " Responces: " + handle.statistics.numberOfResponces +
-       " Backlog: " + (handle.statistics.numberOfRequests - handle.statistics.numberOfResponces) +
-       " Ratio: " + handle.ratio
-     );
-  }
-}
-
-/**
- * @author Martin Tauber <martin_tauber@bmc.com>
- */
-TSPulseAPIDefaultHandler.prototype.handleResponce = function(handle, result) {
-
-  if (result.statusCode >= 400) {
-    logger.error('Status: ' + result.statusCode);
-    logger.error('Headers: ' + JSON.stringify(result.headers));
-    result.setEncoding('utf8');
-    result.on('data', function (body) {
-      logger.error('Body: ' + body);
-    });
-  }
-}
-
-/**
- * @author Martin Tauber <martin_tauber@bmc.com>
- */
-TSPulseAPIDefaultHandler.prototype.handleError = function(error) {
-  logger.error('problem with request: ' + error.message);
-}
 
 /**
  * @author Martin Tauber <martin_tauber@bmc.com>
@@ -282,10 +115,12 @@ var map = {
  tags: ['app_id:Computacenter']
 };
 
+console.log(tsiapi);
 
-pulseAPI = new TSPulseAPI({
+pulseAPI = new tsiapi.tsi({
   email: 'martin_tauber@bmc.com',
-  apiToken: '7cae610a-cf1d-4d20-87f0-61aadea7d8e5'
+  apiToken: '7cae610a-cf1d-4d20-87f0-61aadea7d8e5',
+  logger: logger
 });
 
 process.on('exit', function(){
---
+++ tsiapi.js
@@ -0,0 +1,204 @@
+var https = require('https');
+
+/**
+ * This class implements the interface to TrueSight Pulse. You need to
+ * instanciate this class to be able to communicate with the TrueSight pulse
+ * server. During the instanciatetion you need to specify the options
+ *   email - the email address used to identify at the truesight server
+ *   apiToken - the apiToken used to identify at the truesight server
+ *
+ * optionally you can specify the following options:
+ *   hostname - the hostname of the truesight server
+ *   port - the portnumber of the truesight server
+ *   handler - the handler to be used to handle requests, responces and errors
+ *
+ * @class
+ * @constructor
+ * @param {object} options
+ * @author Martin Tauber <martin_tauber@bmc.com>
+ */
+function TSPulseAPI(options) {
+  this.email = options.email;
+  this.apiToken = options.apiToken;
+  this.hostname = options.hostname != undefined ? options.hostname : 'api.truesight.bmc.com';
+  this.port = options.port != undefined ? options.port : 443;
+  this.handler = options.handler != undefined ? options.handler : new TSPulseAPIDefaultHandler();
+
+  this.maxRetries = options.maxRetries != undefined ? options.maxRetries : 5;
+
+  if (options.logger == undefined) {
+    this.logger = new TSINullLogger();
+  } else {
+    this.logger = options.logger;
+  }
+
+  this.logger.info("Initialized api with the following values:")
+  this.logger.info("email: " + this.email);
+  this.logger.info("apiToken: " + this.apiToken);
+  this.logger.info("hostname: " + this.hostname);
+  this.logger.info("port: " + this.port);
+  this.logger.info("Max retries: " + this.maxRetries);
+
+  this.statistics = {
+    numberOfRequests: 0,
+    numberOfResponces: 0,
+    numberOfErrors: 0,
+    numberOfFatalErrors: 0
+//    firstRequestAt
+//    lastRequestAt
+//    firstResponceAt
+//    lastResponceAt
+  }
+
+  var self=this;
+
+  this.options = {
+    protocol: 'https:',
+    hostname: this.hostname,
+    port: this.port,
+    auth: this.email + ':' + this.apiToken,
+    headers: {
+        'Content-Type': 'application/json',
+    }
+  };
+
+  this._request = function(data) {
+    if (self.statistics.firstRequestAt == undefined)
+      self.statistics.firstRequestAt = new Date();
+
+    self.statistics.lastRequestAt = new Date();
+
+    var retries = 0;
+    var request = https.request(self.options, function(result) {
+      self.statistics.numberOfResponces++;
+      if (self.statistics.firstResponceAt == undefined)
+        self.statistics.firstResponceAt = new Date();
+
+      self.statistics.lastResponceAt = new Date();
+
+      if (result.statusCode >= 400) {
+        self.statistics.numberOfErrors++;
+      }
+
+      self.handler.handleResponce(self, result);
+    });
+
+    request.on('error', function(e) {
+      retries++;
+      if (retries < self.maxRetries) {
+        this.logger.info("Resending request. (retry=" + retries + ")" );
+        self._request(data);
+      } else {
+        self.statistics.numberOfFatalErrors++;
+        self.handler.handleError(self, e);
+      }
+    });
+
+    request.write(JSON.stringify(data));
+    request.end();
+
+    self.statistics.numberOfRequests++;
+    self.handler.handleRequest(self);
+  }
+}
+
+TSPulseAPI.prototype.createEvent = function(data) {
+  this.options.path = '/v1/events';
+  this.options.method = 'POST';
+
+  this._request(data);
+}
+
+/**
+ * @author Martin Tauber <martin_tauber@bmc.com>
+ */
+TSPulseAPI.prototype.createEvents = function(dataProvider, options) {
+  this.logger.info("Creating events ...");
+  if (options == undefined) options = {};
+
+  var min = options.min == undefined ? 50 : options.min;
+  var max = options.max == undefined ? 150 : options.max;
+  var ratio = options.ratio == undefined ? (min + (max - min) / 2) : options.ratio;
+
+  this.logger.info("Minimal backlog: " + min);
+  this.logger.info("Maximal backlog: " + max);
+  this.logger.info("Initial ratio: " + ratio);
+
+  var self = this;
+
+  var worker = function(dataProvider, ratio) {
+    var i = 0;
+    while (i < ratio) {
+      data = dataProvider.next();
+      if (typeof data === 'undefined') break;
+        self.createEvent(data);
+      i++;
+    }
+
+    // selftune the ratio
+    var backlog = self.statistics.numberOfRequests - self.statistics.numberOfResponces;
+    if ( backlog > max ) {
+      ratio = Math.max(0, ratio - (backlog - max));
+    } else if ( backlog + 10 < min + (max - min) / 2) {
+      ratio = ratio + 10;
+    } else if ( backlog - 10 > min + (max - min) / 2) {
+      ratio = ratio - 10;
+    }
+    // this.logger.debug(min + ":" + max + " " + backlog + " " + ratio);
+    self.ratio = ratio;
+
+    if (typeof data !== 'undefined') setTimeout(worker, 1000, dataProvider, ratio);
+  }
+
+  worker(dataProvider, ratio);
+}
+
+
+/**
+ * @author Martin Tauber <martin_tauber@bmc.com>
+ */
+function TSPulseAPIDefaultHandler() {
+}
+
+/**
+ * @author Martin Tauber <martin_tauber@bmc.com>
+ */
+TSPulseAPIDefaultHandler.prototype.handleRequest = function(handle) {
+  if (handle.statistics.numberOfRequests % 1000 == 0) {
+    handle.logger.info("Requests: " + handle.statistics.numberOfRequests +
+       " Responces: " + handle.statistics.numberOfResponces +
+       " Backlog: " + (handle.statistics.numberOfRequests - handle.statistics.numberOfResponces) +
+       " Ratio: " + handle.ratio
+     );
+  }
+}
+
+/**
+ * @author Martin Tauber <martin_tauber@bmc.com>
+ */
+TSPulseAPIDefaultHandler.prototype.handleResponce = function(handle, result) {
+
+  if (result.statusCode >= 400) {
+    handle.logger.error('Status: ' + result.statusCode);
+    handle.logger.error('Headers: ' + JSON.stringify(result.headers));
+    result.setEncoding('utf8');
+    result.on('data', function (body) {
+      handle.logger.error('Body: ' + body);
+    });
+  }
+}
+
+/**
+ * @author Martin Tauber <martin_tauber@bmc.com>
+ */
+TSPulseAPIDefaultHandler.prototype.handleError = function(handle, error) {
+  handle.logger.error('problem with request: ' + error.message);
+}
+
+function TSINullLogger() {
+  this.info = function() {};
+  this.error = function() {};
+  this.debug = function() {};
+}
+
+exports.tsi = TSPulseAPI;
