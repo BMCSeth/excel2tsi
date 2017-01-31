d8f951cfc6fd526271e26c16b44af306d8afbb80
tsguru <martin.tauber@rocketmail.com>
2017-01-31 21:02:07 +0100

    Refactored Project

--- excel2tsi.js
+++ excel2tsi.js
@@ -1,76 +1,12 @@
 const commandLineArgs = require('command-line-args');
 const getUsage = require('command-line-usage')
 
-var XLSX = require('xlsx');
 var log4js = require('log4js');
+var fs = require('fs');
 var logger = log4js.getLogger();
 
-var tsiapi = require('./tsiapi');
-
-/**
- * @author Martin Tauber <martin_tauber@bmc.com>
- */
-function ExcelDataProvider(options) {
-  this.filename = options.filename;
-  this.map = options.map;
-  this.startAt = typeof options.startAt === 'undefined' ? 1 : options.startAt;
-  this.endAt = typeof options.endAt === 'undefined' ? 0 : options.endAt;
-
-  logger.info("Reading Excel file: " + this.filename + " ...");
-  this.workbook = XLSX.readFile(this.filename);
-  this.sheet = this.workbook.Sheets[Object.keys(this.workbook.Sheets)[0]];
-  this.currentRow = this.startAt;
-
-  this.mapper = function(map, sheet, row) {
-    var result = {};
-
-    // loop throw all the entries in the map
-    for(var key in map) {
-      // the content of the map entry is an array
-      if( Object.prototype.toString.call( map[key] ) === '[object Array]' ) {
-        result[key] = [];
-        for (var content in map[key]) {
-          if ( Object.prototype.toString.call( map[key][content] ) === '[object Array]' ) {
-            result[key].push(this.mapper(map[key][content], sheet, row));
-
-          } else if ( typeof map[key][content] === 'object') {
-            result[key].push(this.mapper(map[key][content], sheet, row));
-
-          } else
-            result[key].push(map[key][content]);
-        }
-
-      // the content of this map entry is an object
-      } else if (typeof map[key] === 'object') {
-        result[key] = this.mapper(map[key], sheet, row);
-
-      // the content is neither an object or an array so we will just copy it
-      } else {
-        // TODO needs to be more waterproove
-        if ( /^[A-Z][A-Z]?$/.test(map[key]) ) {
-          result[key] = sheet[map[key] + row].w;
-        } else {
-          result[key] = map[key];
-        }
-      }
-    }
-
-    return result;
-  }
-}
-
-/**
- * @author Martin Tauber <martin_tauber@bmc.com>
- */
-ExcelDataProvider.prototype.next = function() {
-
-  // TODO needs to be more waterproove
-  if (this.sheet['A' + this.currentRow] === undefined
-    || this.sheet['A' + this.currentRow] == ''
-    || (this.endAt != 0 && this.currentRow > this.endAt)) return;
-
-  return this.mapper(this.map, this.sheet, this.currentRow++);
-}
+var TsiAPI = require('./tsiapi').TsiAPI;
+var ExcelDataProvider = require('./ExcelDataProvider').provider;
 
 const optionDefinitions = [
   { name: 'help', alias: 'h', type: Boolean, descritpion:
@@ -145,7 +81,7 @@ if (options.file == undefined) {
 }
 
 if (options.email == undefined) {
-  console.log("please specify an email address used to connect to the TrueSight server.");
+  console.log("Please specify an email address used to connect to the TrueSight server.");
   process.exit(0);
 }
 
@@ -154,57 +90,31 @@ if (options.token == undefined) {
   process.exit(0);
 }
 
-var map = {
-  source: {
-    ref: 'Computacenter',
-    type: 'Computacenter',
-    name: 'Computacenter'
-  },
-  sender: {
-    ref: 'Computacenter',
-    type: 'Computacenter',
-    name: 'Computacenter'
-  },
-  fingerprintFields: ['incident_id'],
-  message: 'N',
-  title: 'N',
-  eventClass: 'Incident',
-  status: 'D',
-  severity: 'E',
-  createdAt: 'A',
-  properties: {
-    app_id: 'Computacenter',
-    incident_id: 'C',
-    u_ge_hr_industry_group: 'B',
-    state: 'D',
-    priority: 'E',
-    contact_type: 'F',
-    city: 'G',
-    country: 'H',
-    opened_at: 'I',
-    sys_created_on: 'J',
-    u_resolved_time: 'K',
-    assignment_group: 'L',
-    reassignment_count: 'M',
-    short_description: 'N',
-    close_notes: 'O',
-    open_ci: 'P',
-    cmdb_ci: 'Q',
-    category: 'R',
-    u_category: 'S',
-    subcategory: 'T',
-    u_subcategory: 'U',
-    made_sla: 'V'
-  },
- tags: ['app_id:Computacenter']
-};
+if (options.token == undefined) {
+  console.log("Please specify a map file.");
+  process.exit(1)
+}
 
-tsi = new tsiapi.tsi({
-  email: 'martin_tauber@bmc.com',
-  apiToken: '7cae610a-cf1d-4d20-87f0-61aadea7d8e5',
+
+// Read the map file
+var map;
+try {
+  map = JSON.parse(fs.readFileSync(options.map, 'utf8'));
+} catch (error) {
+  logger.error("An error occured reading map file ()");
+  logger.error(error);
+  process.exit(1);
+}
+
+
+// create a handle for the TSI server
+tsi = new TsiAPI({
+  email: options.email,
+  apiToken: options.token,
   logger: logger
 });
 
+// when we exit we want to display some statistics
 process.on('exit', function(){
   logger.info("First request at: " + tsi.statistics.firstRequestAt);
   logger.info("Last responce at: " + tsi.statistics.lastResponceAt);
@@ -215,10 +125,13 @@ process.on('exit', function(){
   logger.info("Avg Responces / sec: " + 1000 / (tsi.statistics.lastResponceAt - tsi.statistics.firstRequestAt) * tsi.statistics.numberOfResponces);
 })
 
-var dataProvider = new ExcelDataProvider({
+// create the excel data provider used to collect the data
+var dataProvider = new ExcelDataProvider ({
   filename: options.file,
   map: map,
   startAt : options.start,
+  logger: logger
 });
 
+// create the events using the specified dataprovider
 tsi.createEvents(dataProvider);
